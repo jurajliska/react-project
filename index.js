5145f8c3cf91e41879d2836b6b43ba1cbbcb7eb6
jurajliska <jurinnko@gmail.com>
2024-08-20 17:57:17 +0200

    pridanie charakteru

--- index.js
+++ index.js
@@ -52,7 +52,11 @@ class App extends React.Component {
             "cool": 15
         }
 
-        
+        this.setState(state => { 
+            return {
+                characters: [ ...state.characters, newDude ]
+            }
+        })
     }
     
     // Template
